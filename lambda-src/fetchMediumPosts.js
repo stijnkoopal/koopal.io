dc61127ed7b2c8047622f326fe3aa7335090b32a
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-02 13:52:34 +0200

    Use async/await and isomorphic-fetch for lambda

--- lambda-src/fetchMediumPosts.js
+++ lambda-src/fetchMediumPosts.js
@@ -1,27 +1,24 @@
-import request from 'request'
+import fetch from 'isomorphic-fetch'
 
 const mediumUsername = 'stijn.koopal'
+const mediumUrl = `https://medium.com/@${mediumUsername}/latest?format=json`
 
-export const handler = (event, context, callback) => {
-  const url = `https://medium.com/@${mediumUsername}/latest?format=json`
+const respond = (statusCode, jsonBody) => ({
+  statusCode,
+  headers: {},
+  body: JSON.stringify(jsonBody),
+  isBase64Encoded: false,
+})
 
-  const respond = (mediumError, mediumResponse, mediumBody) => {
-    if (!mediumError && mediumResponse.statusCode === 200) {
-      const jsonBody = JSON.parse(mediumBody.replace('])}while(1);</x>', ''))
-      const posts = Object.values(jsonBody.payload.references.Post)
-
-      const response = {
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify(posts),
-        isBase64Encoded: false,
-      };
-
-      callback(null, response)
-    } else {
-      callback(mediumError)
-    }
+export const handler = async () => {
+  const mediumResponse = await fetch(mediumUrl)
+  if (!mediumResponse.ok) {
+    return respond(503, { error: 'Unable to reach medium' })
   }
 
-  request({ url, json: true }, respond)
+  const responseText = await mediumResponse.text();
+  const json = JSON.parse(responseText.replace('])}while(1);</x>', ''))
+  const posts = Object.values(json.payload.references.Post)
+
+  return respond(200, posts)
 }
