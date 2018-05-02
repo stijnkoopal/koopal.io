b1faaa070f88a0c612d1d6ac9b783d001e27474a
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-02 12:41:40 +0200

    Test function

--- lambda-src/fetchMediumPosts.js
+++ lambda-src/fetchMediumPosts.js
@@ -1,36 +1,27 @@
-const request = require('request')
+import request from 'request'
 
 const mediumUsername = 'stijn.koopal'
-//
-// const handler = (event, context, callback) => {
-//   const url = `https://medium.com/@${mediumUsername}/latest?format=json`
-//
-//   const respond = (mediumError, mediumResponse, mediumBody) => {
-//     if (!mediumError && mediumResponse.statusCode === 200) {
-//       const jsonBody = JSON.parse(mediumBody.replace('])}while(1);</x>', ''))
-//       const posts = Object.values(jsonBody.payload.references.Post)
-//
-//       const response = {
-//         statusCode: 200,
-//         headers: {},
-//         body: JSON.stringify(posts),
-//         isBase64Encoded: false,
-//       };
-//
-//       callback(null, response)
-//     } else {
-//       callback(mediumError)
-//     }
-//   }
-//
-//   request({ url, json: true }, respond)
-// }
-//
-// exports.handler = handler
+
 export const handler = (event, context, callback) => {
-  console.log(event)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({msg: "Hello, World!"})
-  })
+  const url = `https://medium.com/@${mediumUsername}/latest?format=json`
+
+  const respond = (mediumError, mediumResponse, mediumBody) => {
+    if (!mediumError && mediumResponse.statusCode === 200) {
+      const jsonBody = JSON.parse(mediumBody.replace('])}while(1);</x>', ''))
+      const posts = Object.values(jsonBody.payload.references.Post)
+
+      const response = {
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify(posts),
+        isBase64Encoded: false,
+      };
+
+      callback(null, response)
+    } else {
+      callback(mediumError)
+    }
+  }
+
+  request({ url, json: true }, respond)
 }
