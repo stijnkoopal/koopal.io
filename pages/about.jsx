d0b5cd4cb40b0c5b1627a7b906d07fc9edba7110
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-10 16:50:43 +0200

    Add about text

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
 const About = () => (
-  <div>Hoi!</div>
+  <div>
+    <p>
+      I've always had a profound interest for computers and math; be it games, programming, messing around or just searching the great internet.
+      While gaming eventually went away, the rest was there to stay. Computer technology excite me, especially things related to the web.
+    </p>
+
+    <p>
+      After finishing my master in computer science, things started to get less theoretical real fast.
+      I started out as technical engineer in the Java stack.
+      Currently, I have experience in both front- and backend; there aren't a whole lot of languages/platforms/paradigms that I didn't touch.
+    </p>
+
+    <p>
+      In my free time I tend to be outside a lot, especially exercising.
+      Also playing around with my kid and drinking beer with friends is high on the agenda.
+    </p>
+
+    <ul>
+      <li>Are you curious? Contact me!</li>
+      <li>Need someone to help you and your software get to the next level? Contact me!</li>
+      <li>Anything related to sotware development and its process? Contact me!</li>
+      <li>Basically, it comes to this: I can help you!</li>
+    </ul>
+  </div>
 )
 
 export default About
