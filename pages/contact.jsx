9735cf4b18ab8340e557d58d6b9ac1e30d5a94a3
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-10 16:50:11 +0200

    Add contact information

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Contact = () => (
-  <div>Contact!</div>
+  <div>
+    <a href="https://github.com/stijnkoopal" target="_blank" rel="noopener noreferrer">
+      <img src="/static/github-logo.png" alt="Github" title="Check out my Github" />
+    </a>
+    <a href="https://medium.com/@stijn.koopal" target="_blank" rel="noopener noreferrer">
+      <img src="/static/medium-logo.png" alt="Medium" title="Checkout my Medium" />
+    </a>
+    <a href="https://www.linkedin.com/in/stijnkoopal" target="_blank" rel="noopener noreferrer">
+      <img src="/static/linkedin-logo.png" alt="Linkedin" title="Checkout my Linkedin" />
+    </a>
+  </div>
 )
 
 export default Contact
