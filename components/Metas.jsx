0c74684eee868b55c3a8c38ae8f893d9126fc310
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-16 14:49:36 +0200

    Add empty meta tags

--- components/Metas.jsx
+++ components/Metas.jsx
@@ -12,6 +12,18 @@ const Metas = () => (
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
 
       <meta name="robots" content="INDEX,FOLLOW" />
+
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="" />
+      <meta property="og:type" content="" />
+      <meta property="og:url" content="" />
+      <meta property="og:image" content="" />
+
+      <link rel="canonical" href="" />
+      <meta property="author" content="Stijn Koopal" />
     </Head>
   </Fragment>
 )
