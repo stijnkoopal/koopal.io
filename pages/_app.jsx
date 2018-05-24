1286407b6ab3d7c2460ebdf9f5f9a6d9b6880c15
Stijn Koopal <stijn.koopal@gmail.com>
2018-05-24 22:19:13 +0200

    Add proper under construction

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import App, { Container } from 'next/app'
 import React from 'react'
+import Head from 'next/head'
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }) {
@@ -15,6 +16,14 @@ class MyApp extends App {
     const { Component, pageProps } = this.props
     return (
       <Container>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/purecss@1.0.0/build/pure-min.css"
+            integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w"
+            crossOrigin="anonymous"
+          />
+        </Head>
         <Component {...pageProps} />
       </Container>
     )
--- pages/under-construction.jsx
+++ pages/under-construction.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import styled from 'react-emotion'
+
+const CenteredSection = styled('section')`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  width: 40%;
+  padding: 20px;  
+  background: rgba(31,199,255,0.24);
+  color: #444;
+  text-align: center;
+  box-shadow: 0 0 30px rgba(0, 0, 0, 0.2);
+`
 
 const UnderConstruction = () => (
-  <div>Under construction</div>
+  <CenteredSection>
+    <h1>Under construction</h1>
+    <h2>But find me here:</h2>
+
+    <a href="https://github.com/stijnkoopal" target="_blank" rel="noopener noreferrer">
+      <img src="/static/github-logo.png" alt="Github" title="Check out my Github" />
+    </a>
+    <a href="https://medium.com/@stijn.koopal" target="_blank" rel="noopener noreferrer">
+      <img src="/static/medium-logo.png" alt="Medium" title="Checkout my Medium" />
+    </a>
+    <a href="https://www.linkedin.com/in/stijnkoopal" target="_blank" rel="noopener noreferrer">
+      <img src="/static/linkedin-logo.png" alt="Linkedin" title="Checkout my Linkedin" />
+    </a>
+  </CenteredSection>
 )
 
 export default UnderConstruction
