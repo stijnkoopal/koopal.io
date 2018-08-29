7e411eb0a5e571d5c1c36d6b72aaa62d262ffa86
Stijn Koopal <stijn.koopal@gmail.com>
2018-08-29 11:50:03 +0200

    Improve code readability

--- pages/about/components/PieChart.jsx
+++ pages/about/components/PieChart.jsx
@@ -5,45 +5,79 @@ import { hsl } from 'd3-color'
 import { select } from 'd3-selection'
 import * as transition from 'd3-transition'
 
-const pieTop = (d, rx, ry, ir) => {
-  if (d.endAngle - d.startAngle === 0) return 'M 0 0';
-  const sx = rx * Math.cos(d.startAngle)
-  const sy = ry * Math.sin(d.startAngle)
-  const ex = rx * Math.cos(d.endAngle)
-  const ey = ry * Math.sin(d.endAngle)
+const moveTo = (x, y) => `M ${x} ${y}`
+const lineTo = (x, y) => `L ${x} ${y}`
+const arc = (rx, ry, xAxisRotation, largeArcFlag, sweepFlag, x, y) => `A ${rx} ${ry} ${xAxisRotation} ${largeArcFlag} ${sweepFlag} ${x} ${y}`
+const closePath = () => 'z'
 
-  const ret = [];
-  ret.push('M', sx, sy, 'A', rx, ry, '0', (d.endAngle - d.startAngle > Math.PI ? 1: 0), '1', ex, ey, 'L', ir * ex, ir * ey);
-  ret.push('A', ir * rx, ir * ry, '0', (d.endAngle - d.startAngle > Math.PI ? 1: 0), '0', ir * sx, ir * sy, 'z');
-  return ret.join(' ');
-}
+class PieCalculations {
+  constructor(width, height, depth, innerRadius) {
+    this.width = width
+    this.height = height
+    this.depth = depth
+    this.innerRadius = innerRadius
+  }
 
-const pieOuter = (d, rx, ry, h ) => {
-  const startAngle = (d.startAngle > Math.PI ? Math.PI : d.startAngle);
-  const endAngle = (d.endAngle > Math.PI ? Math.PI : d.endAngle);
+  calcXY(startAngle, endAngle, multiplier = 1) {
+    const startX = multiplier * this.width * Math.cos(startAngle)
+    const startY = multiplier * this.height * Math.sin(startAngle)
+    const endX = multiplier * this.width * Math.cos(endAngle)
+    const endY = multiplier * this.height * Math.sin(endAngle)
 
-  const sx = rx * Math.cos(startAngle)
-  const sy = ry * Math.sin(startAngle)
-  const ex = rx * Math.cos(endAngle)
-  const ey = ry * Math.sin(endAngle);
+    return {
+      startX, startY, endX, endY,
+    }
+  }
 
-  const ret = [];
-  ret.push('M', sx, h + sy, 'A', rx, ry, '0 0 1', ex, h + ey, 'L', ex, ey, 'A', rx, ry, '0 0 0', sx, sy, 'z');
-  return ret.join(' ');
-}
+  top(d) {
+    if (d.endAngle - d.startAngle === 0) return moveTo(0, 0);
+    const {
+      startX, startY, endX, endY,
+    } = this.calcXY(d.startAngle, d.endAngle)
 
-const pieInner = (d, rx, ry, h, ir ) => {
-  const startAngle = (d.startAngle < Math.PI ? Math.PI : d.startAngle);
-  const endAngle = (d.endAngle < Math.PI ? Math.PI : d.endAngle);
+    const largeArc = (d.endAngle - d.startAngle > Math.PI ? 1 : 0)
+    return [
+      moveTo(startX, startY),
+      arc(this.width, this.height, 0, largeArc, 1, endX, endY),
+      lineTo(this.innerRadius * endX, this.innerRadius * endY),
+      arc(this.innerRadius * this.width, this.innerRadius * this.height, 0, largeArc, 0, this.innerRadius * startX, this.innerRadius * startY),
+      closePath(),
+    ].join(' ')
+  }
 
-  const sx = ir * rx * Math.cos(startAngle)
-  const sy = ir * ry * Math.sin(startAngle)
-  const ex = ir * rx * Math.cos(endAngle)
-  const ey = ir * ry * Math.sin(endAngle);
+  outer(d) {
+    const startAngle = Math.min(d.startAngle, Math.PI)
+    const endAngle = Math.min(d.endAngle, Math.PI)
 
-  const ret = [];
-  ret.push('M', sx, sy, 'A', ir * rx, ir * ry, '0 0 1', ex, ey, 'L', ex, h + ey, 'A', ir * rx, ir * ry, '0 0 0', sx, h + sy, 'z');
-  return ret.join(' ');
+    const {
+      startX, startY, endX, endY,
+    } = this.calcXY(startAngle, endAngle)
+
+    return [
+      moveTo(startX, this.depth + startY),
+      arc(this.width - 0.5, this.height - 0.5, 0, 0, 1, endX, this.depth + endY),
+      lineTo(endX, endY),
+      arc(this.width - 0.5, this.height - 0.5, 0, 0, 0, startX, startY),
+      closePath(),
+    ].join(' ')
+  }
+
+  inner(d) {
+    const startAngle = Math.max(d.startAngle, Math.PI)
+    const endAngle = Math.max(d.endAngle, Math.PI)
+
+    const {
+      startX, startY, endX, endY,
+    } = this.calcXY(startAngle, endAngle, this.innerRadius)
+
+    return [
+      moveTo(startX, startY),
+      arc(this.innerRadius * (this.width + 0.5), this.innerRadius * (this.height + 0.5), 0, 0, 1, endX, endY),
+      lineTo(endX, this.depth + endY),
+      arc(this.innerRadius * (this.width + 0.5), this.innerRadius * (this.height + 0.5), 0, 0, 0, startX, this.depth + startY),
+      closePath(),
+    ].join(' ')
+  }
 }
 
 const randomData = () => {
@@ -84,11 +118,12 @@ class PieChart extends React.Component {
   draw(group, data) {
     const x = 150
     const y = 150
-    const rx = 130
-    const ry = 100
-    const h = 20
-    const ir = 0
+    const width = 130
+    const height = 100
+    const depth = 20
+    const innerRadius = 0
 
+    const pieCalculations = new PieCalculations(width, height, depth, innerRadius)
     const pieData = pie().value(d => d.value)(data);
 
     const slices = group
@@ -102,7 +137,7 @@ class PieChart extends React.Component {
       .append('path')
       .attr('class', 'innerSlice')
       .style('fill', d => hsl(d.data.color).darker(0.7))
-      .attr('d', d => pieInner(d, rx + 0.5, ry + 0.5, h, ir))
+      .attr('d', d => pieCalculations.inner(d))
 
     slices.selectAll('.topSlice')
       .data(pieData)
@@ -111,7 +146,7 @@ class PieChart extends React.Component {
       .attr('class', 'topSlice')
       .style('fill', d => d.data.color)
       .style('stroke', d => d.data.color)
-      .attr('d', d => pieTop(d, rx, ry, ir))
+      .attr('d', d => pieCalculations.top(d))
 
     slices.selectAll('.outerSlice')
       .data(pieData)
@@ -119,15 +154,15 @@ class PieChart extends React.Component {
       .append('path')
       .attr('class', 'outerSlice')
       .style('fill', d => hsl(d.data.color).darker(0.7))
-      .attr('d', d => pieOuter(d, rx - 0.5, ry - 0.5, h))
+      .attr('d', d => pieCalculations.outer(d))
 
     slices.selectAll('.percent')
       .data(pieData)
       .enter()
       .append('text')
       .attr('class', 'percent')
-      .attr('x', d => 0.6 * rx * Math.cos(0.5 * (d.startAngle + d.endAngle)))
-      .attr('y', d => 0.6 * ry * Math.sin(0.5 * (d.startAngle + d.endAngle)))
+      .attr('x', d => 0.6 * width * Math.cos(0.5 * (d.startAngle + d.endAngle)))
+      .attr('y', d => 0.6 * height * Math.sin(0.5 * (d.startAngle + d.endAngle)))
       .text('test')
   }
 }
