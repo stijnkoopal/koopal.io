8525296bdc2767de5f84c7241c73ebff45b62d94
Stijn Koopal <stijn.koopal@gmail.com>
2018-09-19 16:24:28 +0200

    Add proper position to text

--- pages/_components/Layout.jsx
+++ pages/_components/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'react-emotion'
+import { Box } from '@rebass/grid/emotion'
 import Menu from './Menu'
 
 const OuterContainer = styled('div')`
@@ -9,17 +10,23 @@ const OuterContainer = styled('div')`
   height: 100%;
 `
 
-const Main = styled('div')`
-  width: 30%;
-  height: 100%;
-  margin: 0 auto;
-`
+const Main = props => (
+  <Box
+    {...props}
+    mx="auto"
+    width={[1, 1 / 2]}
+    css={{
+      maxWidth: '1400px',
+      height: '100%',
+    }}
+  />
+)
 
 const Layout = ({ children }) => (
   <OuterContainer id="outer-container">
     <Menu pageWrapId="page-wrap" outerContainerId="outer-container" />
     <Main id="page-wrap">
-      { children }
+      {children}
     </Main>
   </OuterContainer>
 )
--- pages/home/Home.jsx
+++ pages/home/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Flex, Box } from '@rebass/grid/emotion'
+import { withTheme } from 'emotion-theming'
 
-const Home = () => (
-  <div>
-    Welcome! My name is Stijn, I'm a Full Stack Software Enthusiast.
-  </div>
+const Home = ({ theme: { typography } }) => (
+  <Flex alignItems="center" justifyContent="center" css={{ height: '100%' }}>
+    <Box css={{ ...typography.display2 }}>
+      Welcome! My name is Stijn, I'm a Full Stack Software Enthusiast.
+    </Box>
+  </Flex>
 )
 
-export default Home
+export default withTheme(Home)
