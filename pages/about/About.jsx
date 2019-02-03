c470e9e470d683346aec3b73423d2a0a381c2600
Stijn Koopal <stijn.koopal@gmail.com>
2019-02-03 21:01:34 +0100

    Initial commit of 3d bar chart

--- pages/about/About.jsx
+++ pages/about/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Box, Flex } from '@rebass/grid/emotion'
 import styled from '@emotion/styled'
-import ResponsiveRadarChart from './components/ResponsiveRadarChart'
+import ThreeDBarChart from './components/ThreeDBarChart'
 
 const PageIndicator = styled(Box)(({ active, theme: { palette, spacing } }) => ({
   borderRadius: '50%',
@@ -104,7 +104,7 @@ class About extends React.Component {
   render() {
     return (
       <>
-        <ResponsiveRadarChart data={chartData[this.state.activeChart]} />
+        <ThreeDBarChart />
 
         <Arrow onClick={this.nextActiveChart} />
 
