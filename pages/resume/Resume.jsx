da86e50b174fbe28db87e2f9a12621c9cf938425
Stijn Koopal <stijn.koopal@gmail.com>
2019-02-27 10:54:46 +0100

    Proper animations on resume page

--- pages/resume/Resume.jsx
+++ pages/resume/Resume.jsx
@@ -27,11 +27,15 @@ const ConnectionLine = styled.div(({ color }) => ({
   zIndex: 1,
 }))
 
-const ProjectVisualization = styled(Flex)(({ isOpen, backgroundColor, theme: { spacing, transitions } }) => ({
+const ProjectVisualization = styled(Flex)(({ isOpen, odd, backgroundColor, theme: { spacing, transitions } }) => ({
   flexDirection: isOpen ? 'column' : 'row',
   borderRadius: isOpen ? '5%' : '50%',
-  width: isOpen ? '30vh' : '20vh',
-  height: isOpen ? '30vh' : '20vh',
+  width: isOpen ? '100%' : '25vw',
+  maxWidth: isOpen ? '99999px' : '200px',
+  height: isOpen ? 'unset' : '25vw',
+  maxHeight: isOpen ? '99999px' : '200px',
+  flex: isOpen ? 1 : 'unset',
+  margin: odd ? `0 0 0 ${spacing.unit}px` : `0 ${spacing.unit}px 0 0`,
   backgroundColor,
   zIndex: 2,
   padding: (isOpen ? 1 : 2) * spacing.unit,
@@ -47,13 +51,17 @@ const ProjectVisualizationContainer = styled(Flex)({
   position: 'relative',
 })
 
-const ProjectWrapper = styled(Flex)(({ odd, theme: { spacing } }) => ({
+const ProjectWrapper = styled(Flex)(({ chevronPosition, odd, theme: { spacing, transitions } }) => ({
   height: '20vh',
   alignItems: 'center',
-  justifyContent: 'space-around',
+  justifyContent: odd ? 'flex-start' : 'flex-end',
   flexDirection: odd ? 'row' : 'row-reverse',
-  '& > *:nth-of-type(1), & > *:nth-of-type(3)': {
-    width: `calc((100% - ${spacing.unit * 2}px) / 2)`,
+  [`& > *:nth-of-type(${odd ? 1 : 3})`]: {
+    transition: `all ${transitions.duration.short}ms linear`,
+    width: `calc((100% - ${spacing.unit * 2}px) / ${chevronPosition === 'left' ? 2.3 : chevronPosition === 'right' ? 1.7 : 2})`,
+  },
+  [`& > *:nth-of-type(${odd ? 3 : 1})`]: {
+    flex: 1,
   },
   marginTop: spacing.unit,
   [ProjectVisualizationContainer]: {
@@ -110,26 +118,26 @@ const ChevronWithText = ({ children, color }) => (
   </Chevron>
 )
 
-const ProjectDescription = ({ project }) => {
-  console.log(project)
-  return (
-    <>
-      <h2>{project.name}{project.via && ` via ${project.via}`}</h2>
-      <div>{project.description}</div>
-    </>
-  )
-}
+const ProjectDescription = ({ project }) => (
+  <>
+    <h2>
+      {project.name}
+      {project.via && ` via ${project.via}`}
+    </h2>
+    <div>{project.description}</div>
+  </>
+)
 
-const Project = ({ project, odd, color, isOpen, onClick }) => {
+const Project = ({ chevronPosition, project, odd, color, isOpen, onClick }) => {
   const visualization = (
-    <ProjectVisualization backgroundColor={color} onClick={onClick} isOpen={isOpen}>
+    <ProjectVisualization odd={odd} backgroundColor={color} onClick={onClick} isOpen={isOpen}>
       {!isOpen && <Logo src={project.entityIconUrl} alt={`${project.entity} logo`}/>}
       {isOpen && <ProjectDescription project={project}/>}
     </ProjectVisualization>
   )
 
   return (
-    <ProjectWrapper odd={odd}>
+    <ProjectWrapper odd={odd} chevronPosition={chevronPosition}>
       <ProjectDates color={color} odd={odd}>
         {formatDate(project.startDate)} - {formatDate(project.endDate)}
       </ProjectDates>
@@ -154,16 +162,18 @@ const Resume = ({
                   },
                 }) => {
   const projects = resume.projects.sort((a, b) => (a.startDate < b.startDate ? 1 : -1))
-  const [selectedProject, selectProject] = useState(undefined)
+  const [selectedProjectIndex, selectProject] = useState(undefined)
+  const chevronPosition = selectedProjectIndex === undefined ? 'center' : selectedProjectIndex % 2 === 0 ? 'right' : 'left'
 
   return (
     <ProjectList>
       {projects.map((project, index) => (
         <Project
-          onClick={() => selectProject(selectedProject === project ? undefined : project)}
+          chevronPosition={chevronPosition}
+          onClick={() => selectProject(selectedProjectIndex === index ? undefined : index)}
           odd={index % 2}
           key={project.name}
-          isOpen={selectedProject === project}
+          isOpen={selectedProjectIndex === index}
           project={project}
           color={colors.visualizations[index]}
         />
